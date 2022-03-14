5438fc67eb26c2d8d2808e7c430ff2a5ac45f7ab
Christian <chrnielsen2003@gmail.com>
2022-03-14 21:51:17 +0100

    .

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,13 +5,13 @@ import "firebase/compat/firestore"
 
 const app = firebase.initializeApp({
     
-    apiKey: "AIzaSyC3UPwqSIYRm6t-RmFFdi-Lfttp7YHkXvw",
-  authDomain: "tento-92d06.firebaseapp.com",
-  projectId: "tento-92d06",
-  storageBucket: "tento-92d06.appspot.com",
-  messagingSenderId: "21821140314",
-  appId: "1:21821140314:web:3b38b054a78a003961d763",
-  measurementId: "G-4C9N6B24XT"
+    apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
     
 });
 
